29948d5e915a81ff73a96b746631ea7a7ce73a36
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-23 15:00:52 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -3,8 +3,19 @@ import '../styles/App.css';
 
 const App = () => {
   return (
-    <div id="main"></div>
-  )
+    <>
+    <div id="main">
+    <h1>Work-Time</h1>
+    <button data-testid='start-btn'>start</button>
+    <button data-testid='stop-btn'>Stop</button>
+    <button data-testid='reset-btn'>Reset</button>
+    <input data-testid='work-duration'>
+    </input>
+    <input data-testid='break-duration'></input>
+    <button data-testid='set-btn'>Set</button>
+    </div>
+    </>
+  );
 }
 
 
