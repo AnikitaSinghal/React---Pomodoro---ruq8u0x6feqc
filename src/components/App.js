96c22aef6f2ac3caf1305bb5543db6603e175190
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-23 15:35:40 +0530

    completed

--- src/components/App.js
+++ src/components/App.js
@@ -5,13 +5,12 @@ const App = () => {
   return (
     <>
     <div id="main">
-      <h1>25:00</h1>
     <h1>Work-Time</h1>
     <button data-testid='start-btn'>start</button>
     <button data-testid='stop-btn'>Stop</button>
     <button data-testid='reset-btn'>Reset</button>
-    <input data-testid='work-duration'>25</input>
-    <input data-testid='break-duration'>5</input>
+    <input data-testid='work-duration'></input>
+    <input data-testid='break-duration'></input>
     <button data-testid='set-btn'>Set</button>
     </div>
     </>
