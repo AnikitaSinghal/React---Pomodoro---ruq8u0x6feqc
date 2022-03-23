1ffaff2c3c70b448ad4aac1a0258423ab6ea10ac
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-03-23 15:29:05 +0530

    complete

--- src/components/App.js
+++ src/components/App.js
@@ -4,20 +4,15 @@ import '../styles/App.css';
 const App = () => {
   return (
     <>
-    <div id="main">
+    <div id="main"></div>
       <h1>25:00</h1>
     <h1>Work-Time</h1>
-    <button data-testid='start-btn'
-    onClick={{
-      document.getElementById("stop-btn").disabled=true;
-      document.getElementById("reset-btn").disabled=true
-    }}>start</button>
+    <button data-testid='start-btn'>start</button>
     <button data-testid='stop-btn'>Stop</button>
     <button data-testid='reset-btn'>Reset</button>
     <input data-testid='work-duration'>25</input>
     <input data-testid='break-duration'>5</input>
     <button data-testid='set-btn'>Set</button>
-    </div>
     </>
   );
 }
