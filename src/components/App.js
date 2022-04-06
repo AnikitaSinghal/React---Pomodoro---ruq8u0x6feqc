184550e7e92ce8c9c292d57d4ecbb7683f81937b
Anikita Singhal <92014545+AnikitaSinghal@users.noreply.github.com>
2022-04-06 19:56:59 +0530

    d

--- src/components/App.js
+++ src/components/App.js
@@ -6,9 +6,7 @@ const App = () => {
     <>
     <div id="main">
     <h1>Work-Time</h1>
-    <button data-testid='start-btn' onclick={()=>{
-      disbled=true
-    }}>start</button>
+    <button data-testid='start-btn'>start</button>
     <button data-testid='stop-btn'>Stop</button>
     <button data-testid='reset-btn'>Reset</button>
     <input data-testid='work-duration'>25:00</input>
